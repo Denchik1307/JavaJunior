dce71ae6adbc3527e90e3519fb740df8564252f9
vazhnik <denis13071981@gmail.com>
2024-09-11 09:45:34 +0300

    homework_two

--- src/main/java/homework_two/Main.java
+++ src/main/java/homework_two/Main.java
@@ -1,9 +1,6 @@
 package homework_two;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.TypeVariable;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
