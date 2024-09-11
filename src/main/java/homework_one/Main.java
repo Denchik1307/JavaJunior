7aa45a6f2f0fb003e5dde3a18cad45e89431a0fa
vazhnik <denis13071981@gmail.com>
2024-09-11 08:23:08 +0300

    homework_one

---
+++ src/main/java/homework_one/Main.java
@@ -0,0 +1,18 @@
+package homework_one;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        System.out.println(averageOfEventFromList(numbers));
+    }
+    private static double averageOfEventFromList(List<Integer> list){
+        return list.stream().filter(num -> num%2 ==0)
+                .mapToDouble(num -> num)
+                .average()
+                .orElse(0f);
+    }
+}
