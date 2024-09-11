57f0de50898f9277750252ddb6e215fb68b22fa1
vazhnik <denis13071981@gmail.com>
2024-09-11 09:45:06 +0300

    homework_two

---
+++ src/main/java/homework_two/Main.java
@@ -0,0 +1,37 @@
+package homework_two;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.TypeVariable;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Class<String> clazz = String.class;
+        Method[] methods = clazz.getDeclaredMethods();
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Method method : methods) {
+            StringBuilder tmp = new StringBuilder();
+            Class<?>[] params = method.getParameterTypes();
+            for (Class<?> p : params) {
+                tmp.append(p.getSimpleName()).append(", ");
+            }
+            String parameters = tmp.length() < 2 ? tmp.toString() : tmp.substring(0, tmp.length() - 2);
+
+            stringBuilder.append("\u001B[32m")
+                    .append(method.getName())
+                    .append("\u001B[34m")
+                    .append("(")
+                    .append(parameters)
+                    .append(")")
+                    .append("\u001B[31m return:")
+                    .append(method.getReturnType().getSimpleName())
+                    .append("\n");
+        }
+        System.out.println(stringBuilder);
+
+    }
+}
